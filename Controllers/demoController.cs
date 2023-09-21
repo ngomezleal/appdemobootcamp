583254a10d815a6eb2715edd3952ebed3f590873
Nelson Gomez <ngomezleal@gmail.com>
2023-09-21 16:36:01 -0500

    Demo 2

---
+++ Controllers/demoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appdemobootcamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class demoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { id = 1, name = "nelson" });
+        }
+    }
+}
